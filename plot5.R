342c72ca835720fc0da45d046565f96522684717
Gradon <gradon.kam@gmail.com>
2020-10-25 08:40:22 -0500

    fix typos

--- plot5.R
+++ plot5.R
@@ -11,13 +11,13 @@ baltemissions <- subset(NEIdata, fips == 24510)
 # on-road and non-road sources are motor vehicles, can just subset by type instead of SCC code
 baltmotors <- subset(baltemissions, type == "ON-ROAD" | type == "NON-ROAD")
 
-# total Baltimore City motor vehicle data data for each year
+# total Baltimore City motor vehicle data for each year
 totalbaltmotors <- with(baltmotors, tapply(Emissions, year, sum))
 
 # open file device for plotting
 png(filename = "plot5.png", width = 480, height = 480)
 
-# plot coal emissions
+# plot total emissions
 plot(names(totalbaltmotors), totalbaltmotors, xlab = "Year", ylab = "PM2.5", pch = 20, main = "Motor Vehicle-Related PM2.5 Emissions in Baltimore City by Year")
 
 # close png device
